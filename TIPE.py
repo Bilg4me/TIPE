de596c99026a8e4a05c6525ff2bcfc28f70e8084
Bilg4me <bilgame91330@gmail.com>
2020-04-05 15:28:32 +0200

    Ajout des script python

---
+++ TIPE.py
@@ -0,0 +1,169 @@
+from ressources import *
+
+def Mat(n):
+    return [[ZERO for j in range(n)] for i in range(n)]
+    
+def Taille(M):
+    return len(M)
+    
+def Noeuds(G):
+	N = []
+	for P,Points in G:
+		if not P in N:
+			N.append(P)
+		for M,poids in Points:
+			if not M in N:
+				N.append(M)
+	return [Alphabet[i] for i in range(len(N))]
+
+def GrapheVersMat(G):
+    A = Annuaire()
+    N = Noeuds(G)
+    Adj = Mat(len(N))
+    for (P,Points) in G:
+        for (s,l) in Points:
+            Adj[A[P]][A[s]] = l
+            Adj[A[s]][A[P]] = l
+    return Adj
+
+def Autour(S, Adj):
+    S_ = Annuaire()[S]
+    Autour = []
+    Points = Adj[S_]
+    for i,poids in enumerate(Points):
+        if poids > ZERO :
+            Autour.append((Alphabet[i], poids))
+    return Autour
+
+def PlusProche(Lignes):
+	M,m = None,INF
+	for k in range(len(Lignes[-1])) :
+		
+		if type(Lignes[-1][k]) == tuple and Lignes[-1][k][1] < m:
+			
+			M,m = Alphabet[k],Lignes[-1][k][1]
+			
+	return M,m
+
+def Retirer(P, Points):
+	for k in range(len(Points)):
+		if P == Points[k]:
+			Points.pop(k)
+			break
+
+def Init(M, G):
+	A = GrapheVersMat(G)
+	T = [(M,ZERO)]
+	R = Noeuds(G)
+	LL = []
+	L = [Noeuds(G)]
+	for P in R:
+		if P == M:
+			LL.append(ZERO)
+		else:
+			LL.append(INF)
+	L.append(LL)
+	return L, T, R, A
+
+def Tableau(M, G):
+	
+	Lignes ,Trouve, Recherche, Adj = Init(M,G)
+	
+	def Placer(O,Voisins,acc):
+		L = []
+		PointsTrouves = [P for P,poids in Trouve]
+		PointsVoisins = [P for P,poids in Voisins]
+		Noeuds = Lignes[0]
+
+		for M in Noeuds:
+			if M in PointsTrouves:
+				L.append(None)
+			elif M in PointsVoisins:
+				for P,poids in Voisins:
+					if P == M and type(Lignes[-1][Annuaire()[M]]) == tuple:
+						if Lignes[-1][Annuaire()[M]][1] < acc + poids:
+							L.append((Lignes[-1][Annuaire()[M]]))
+						else:
+							L.append((O, acc + poids))
+					elif P == M and Lignes[-1][Annuaire()[M]] == INF:
+						L.append((O, acc + poids))
+						
+			else:
+				L.append(Lignes[-1][Annuaire()[M]])
+				
+		Lignes.append(L)
+		Trouve.append(PlusProche(Lignes))
+		Retirer(O, Recherche)
+
+	
+	while len(Recherche) > 1:
+		M = Trouve[-1][0]
+		acc = Trouve[-1][1]
+		V = Autour(M, Adj)
+		Placer(M,V,acc)
+		# print("je pars de",M,"accumulateur",acc)
+		
+	return Lignes, Trouve
+
+def BienAfficher(L):
+	disp = ""
+	for l in L:
+		disp += str(l) + '\n'
+	
+	print(disp)
+
+def Suivis(A,B,L):
+	for k in range(len(L)-1):
+		if ( L[k] == A and L[k+1] == B ) or ( L[k] == B and L[k+1] == A ):
+			return True
+	return False
+
+def Visualiser(A,B,G,barycentre):
+	print("plus court chemin entre {} et {}".format(A,B))
+	print("Conditions de PCC1 sont t : {0} et d: {1} en vert".format(barycentre ,1 - barycentre))
+	print("Conditions du PCC2 sont t : {0} et d: {1} en rouge".format(1 - barycentre, barycentre))
+	Adj = GrapheVersMat(G)
+	g = Graph(engine='circo')
+	PCC1,PCC2 = Chemin(A,B,G,barycentre),Chemin(A,B,G,1 - barycentre)
+	for i in range(len(Adj)):
+		for j in range(i+1,len(Adj)):
+			if Adj[i][j] > ZERO:
+				if Suivis(Alphabet[j],Alphabet[i],PCC1) :
+					g.edge( Alphabet[j] , Alphabet[i] ,label = str(Adj[i][j]) + "PCC1" , color='green', penwidth='2')
+				elif Suivis(Alphabet[j],Alphabet[i],PCC2) :
+					g.edge( Alphabet[j] , Alphabet[i] ,label = str(Adj[i][j]) + "PCC2" , color='red', penwidth='2')
+				else:
+					g.edge( Alphabet[j] , Alphabet[i] ,label = str(Adj[i][j]) , color='black')
+	
+	g.view()
+			
+def min_of(L):
+	M,m = None,INF
+	for k in L:
+		if type(k) == tuple and k[1] < m:
+			M,m = k
+	return M
+	
+def Chemin(A,B,G,barycentre):
+	Poids.λ = barycentre
+	Optimal = [B]
+	Lignes,Trouve = Tableau(A,G)
+	while B != A:
+		for k in range(len(Trouve)):
+			if Trouve[k][0] == B :
+				B = min_of(Lignes[k+1])
+				Optimal = [B] + Optimal
+	return Optimal
+
+###################
+## PHASE DE TEST ##
+##################
+
+oui = 1
+while oui:
+	Visualiser('A',chr(65+randint(1,13)),Abstrait,1)
+	oui = int(input("continuer ?"))
+
+
+
+
---
+++ ressources.py
@@ -0,0 +1,128 @@
+
+from math import inf
+from random import randint
+from graphviz import Graph, Source
+
+
+
+class Poids:
+	
+	λ = None
+	
+	def __init__(self, time, dist):
+		self.temps = time
+		self.distance = dist
+	
+	def __add__(self, w):
+		p = Poids(0,0)
+		p.distance = self.distance + w.distance
+		p.temps = self.temps + w.temps
+		return p
+	
+	def __iadd__(self, w):
+		return self + w
+		
+	def __lt__(self,w):
+		if w.temps == inf:
+			return True 
+		return  Poids.λ * self.temps + (1-Poids.λ) * self.distance < Poids.λ * w.temps + (1-Poids.λ) * w.distance
+	
+	def __eq__(self,w):
+		if w.temps == inf and self.temps == inf:
+			return True
+		elif w.temps == inf and self.temps != inf:
+			return False
+		else:
+			 return Poids.λ * self.temps + (1-Poids.λ) * self.distance == Poids.λ * w.temps + (1-Poids.λ) * w.distance
+		
+	def __le__(self,w):
+		return self < w or self == w
+		
+	def __gt__(self,w):
+		return not (self <= w)
+	
+	def __ge__(self,w):
+		return not (self < w)
+	
+	def __str__(self):
+		return "(t{0},d{1})".format(self.temps, self.distance)
+
+ZERO = Poids(0,0)
+INF = Poids(inf,inf)
+
+Alphabet = [ chr(65+i) for i in range(50) ]
+
+def Annuaire():
+    A = {}
+    for i in range(50):
+        A[chr(65 + i)] = i
+    return A
+    
+P = lambda : Poids(randint(1,6),randint(1,6))
+
+Pays = [
+('A',[('B',P()),('E',P())]),
+('B',[('D',P())]),
+('C',[('B',P()),('D',P()),('E',P())]),
+('F',[('D',P()),('E',P())])
+]
+
+
+Ville = [
+('A', [('B', P()),('C', P()), ('D', P())]),
+('D', [('B', P()),('C', P())])
+]
+
+def Normaliser(s):
+	g = ""
+	build = ""
+	for k in range(len(s)):
+		
+		if s[k].isnumeric():
+			build += s[k]
+		elif build != "":
+			if int(build) > 15:
+				g += str(randint(0,15)) + s[k]
+			else:
+				g+= build + s[k]
+			build = ""
+		else:
+			g += s[k]
+	return g
+
+def Lettrifier(s):
+	g = ""
+	build = ""
+	for k in range(len(s)):
+		if s[k].isnumeric():
+			build += s[k]
+		elif build != "":
+			g += Alphabet[int(build)] + s[k]
+			build = ""
+		else:
+			g += s[k]
+	return g
+
+def DotVersGraphe(content):
+	G = []
+	L = content.split('\n\t')
+	L = L[1:-1]
+	for l in Alphabet:
+		Points = []
+		for t in L:
+			if t[0] == l:
+				Points.append((t[-1],P()))
+		if Points != []:
+			G.append( (l,Points) )
+	return G
+
+path = 'abstract.gv'
+dot = Source.from_file(path)	
+dot.source = Lettrifier(Normaliser(dot.source))
+Abstrait = DotVersGraphe(dot.source)
+
+
+
+
+
+

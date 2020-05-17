38b914884f9f32248d9e54896287e4ad2515ebaf
brome <bilgame91330@gmail.com>
2020-05-17 16:07:28 +0200

    Ajout du Fork Deterministe Dynamique

--- TIPE.py
+++ TIPE.py
@@ -1,31 +1,5 @@
 from ressources import *
 
-def Mat(n):
-    return [[ZERO for j in range(n)] for i in range(n)]
-    
-def Taille(M):
-    return len(M)
-    
-def Noeuds(G):
-	N = []
-	for P,Points in G:
-		if not P in N:
-			N.append(P)
-		for M,poids in Points:
-			if not M in N:
-				N.append(M)
-	return [Alphabet[i] for i in range(len(N))]
-
-def GrapheVersMat(G):
-    A = Annuaire()
-    N = Noeuds(G)
-    Adj = Mat(len(N))
-    for (P,Points) in G:
-        for (s,l) in Points:
-            Adj[A[P]][A[s]] = l
-            Adj[A[s]][A[P]] = l
-    return Adj
-
 def Autour(S, Adj):
     S_ = Annuaire()[S]
     Autour = []
@@ -38,11 +12,8 @@ def Autour(S, Adj):
 def PlusProche(Lignes):
 	M,m = None,INF
 	for k in range(len(Lignes[-1])) :
-		
 		if type(Lignes[-1][k]) == tuple and Lignes[-1][k][1] < m:
-			
 			M,m = Alphabet[k],Lignes[-1][k][1]
-			
 	return M,m
 
 def Retirer(P, Points):
@@ -66,9 +37,9 @@ def Init(M, G):
 	return L, T, R, A
 
 def Tableau(M, G):
-	
+
 	Lignes ,Trouve, Recherche, Adj = Init(M,G)
-	
+
 	def Placer(O,Voisins,acc):
 		L = []
 		PointsTrouves = [P for P,poids in Trouve]
@@ -84,33 +55,27 @@ def Tableau(M, G):
 						if Lignes[-1][Annuaire()[M]][1] < acc + poids:
 							L.append((Lignes[-1][Annuaire()[M]]))
 						else:
-							L.append((O, acc + poids))
+							L.append((O, acc + poids)) # attention la loi + pour les poids n'est pas commutative
 					elif P == M and Lignes[-1][Annuaire()[M]] == INF:
 						L.append((O, acc + poids))
 						
+
 			else:
 				L.append(Lignes[-1][Annuaire()[M]])
-				
+
 		Lignes.append(L)
 		Trouve.append(PlusProche(Lignes))
 		Retirer(O, Recherche)
 
-	
+
 	while len(Recherche) > 1:
 		M = Trouve[-1][0]
 		acc = Trouve[-1][1]
+		print("je me trouve en",M,"j'ai accumule",acc, "->" , acc.valeur)
 		V = Autour(M, Adj)
 		Placer(M,V,acc)
-		# print("je pars de",M,"accumulateur",acc)
-		
-	return Lignes, Trouve
 
-def BienAfficher(L):
-	disp = ""
-	for l in L:
-		disp += str(l) + '\n'
-	
-	print(disp)
+	return Lignes,Trouve
 
 def Suivis(A,B,L):
 	for k in range(len(L)-1):
@@ -118,34 +83,30 @@ def Suivis(A,B,L):
 			return True
 	return False
 
-def Visualiser(A,B,G,barycentre):
+
+def Visualiser(A,B,G):
 	print("plus court chemin entre {} et {}".format(A,B))
-	print("Conditions de PCC1 sont t : {0} et d: {1} en vert".format(barycentre ,1 - barycentre))
-	print("Conditions du PCC2 sont t : {0} et d: {1} en rouge".format(1 - barycentre, barycentre))
 	Adj = GrapheVersMat(G)
-	g = Graph(engine='circo')
-	PCC1,PCC2 = Chemin(A,B,G,barycentre),Chemin(A,B,G,1 - barycentre)
+	g = Graph(engine='sfdp')
+	PCC1 = Chemin(A,B,G)
 	for i in range(len(Adj)):
 		for j in range(i+1,len(Adj)):
 			if Adj[i][j] > ZERO:
 				if Suivis(Alphabet[j],Alphabet[i],PCC1) :
-					g.edge( Alphabet[j] , Alphabet[i] ,label = str(Adj[i][j]) + "PCC1" , color='green', penwidth='2')
-				elif Suivis(Alphabet[j],Alphabet[i],PCC2) :
-					g.edge( Alphabet[j] , Alphabet[i] ,label = str(Adj[i][j]) + "PCC2" , color='red', penwidth='2')
+					g.edge( Alphabet[j] , Alphabet[i] ,label = str(Adj[i][j]), color='green', penwidth='2')
 				else:
 					g.edge( Alphabet[j] , Alphabet[i] ,label = str(Adj[i][j]) , color='black')
-	
 	g.view("Graphe.gv", "Graphe")
-			
+    
+
 def min_of(L):
 	M,m = None,INF
 	for k in L:
 		if type(k) == tuple and k[1] < m:
 			M,m = k
 	return M
-	
-def Chemin(A,B,G,barycentre):
-	Poids.λ = barycentre
+
+def Chemin(A,B,G):
 	Optimal = [B]
 	Lignes,Trouve = Tableau(A,G)
 	while B != A:
@@ -157,13 +118,6 @@ def Chemin(A,B,G,barycentre):
 
 ###################
 ## PHASE DE TEST ##
-##################
+###################
 
-oui = 1
-while oui:
-	Visualiser('A',chr(65+randint(1,13)),Abstrait,1)
-	oui = int(input("continuer ?"))
-
-
-
-
+Visualiser('A','F', Pays)
--- ressources.py
+++ ressources.py
@@ -2,50 +2,104 @@ from math import inf
 from random import randint
 from graphviz import Graph, Source
 
+## TODO: Travailler avec un graphe FIFO
+## TODO: Créer la classe Poids Dynamique 
+## Methode : maj, add, eq, lt, str, float, ...
+## Attribut : Uplet
+## Variable de classe : Horloge, Periode, Index
+
+# IDEES : Une methode statique ou de classe qui stockerait les Poids crées dans une db et qui pourrait tous les mettre à jour
+# avec un methode de statique ou de classe qui les mettrait à jour
+
+# Questionnement : est ce que horloge et index ne devrait-il pas être un attribut plutot qu'une variable de classe ?
+# ne serait-ce que pour bien afficher les poids lors du graph final
+
+def Mat(n):
+    return [[ZERO for j in range(n)] for i in range(n)]
+
+def Taille(M):
+    return len(M)
+
+def Noeuds(G):
+	N = []
+	for P,Points in G:
+		if not P in N:
+			N.append(P)
+		for M,poids in Points:
+			if not M in N:
+				N.append(M)
+	return [Alphabet[i] for i in range(len(N))]
+
+def GrapheVersMat(G):
+    A = Annuaire()
+    N = Noeuds(G)
+    Adj = Mat(len(N))
+    for (P,Points) in G:
+        for (s,l) in Points:
+            Adj[A[P]][A[s]] = l
+            Adj[A[s]][A[P]] = l
+    return Adj
+
 class Poids:
 	
-	λ = None
+	# Variable de classe
+	NBelement = 3
+	Periode = 12 // NBelement
 	
-	def __init__(self, time, dist):
-		self.temps = time
-		self.distance = dist
-	
-	def __add__(self, w):
-		p = Poids(0,0)
-		p.distance = self.distance + w.distance
-		p.temps = self.temps + w.temps
-		return p
-	
+	def __init__(self, liste_temps):
+		self.collection = liste_temps
+		self.horloge = 0
+		self.index = (self.horloge // Poids.Periode) % Poids.NBelement
+		self.valeur = self.collection[self.index]
+		
+	def sethorloge(self, h):
+		self.horloge = h
+		self.index = (self.horloge // Poids.Periode) % Poids.NBelement
+		self.valeur = self.collection[self.index]
+		
+	def __getitem__(self, index):
+		return self.collection[index]
+		
+	def __add__(self, poids):
+		# ici self est un accumulateur de sorte qu'on défini ici une loi + de la forme acc + poids
+		
+		if self == Poids([0,0,0]):
+			return poids
+		
+		collectionSomme = []
+
+		for valeur in self:
+			v = self.horloge + valeur
+			index = (v // Poids.Periode) % Poids.NBelement
+			collectionSomme.append(v + poids[index])
+				
+		P = Poids(collectionSomme)
+		P.sethorloge(self.horloge)
+		return P
+
 	def __iadd__(self, w):
 		return self + w
-		
+
 	def __lt__(self,w):
-		if w.temps == inf:
-			return True 
-		return  Poids.λ * self.temps + (1-Poids.λ) * self.distance < Poids.λ * w.temps + (1-Poids.λ) * w.distance
-	
+		return  self.valeur < w.valeur
+
 	def __eq__(self,w):
-		if w.temps == inf and self.temps == inf:
-			return True
-		elif w.temps == inf and self.temps != inf:
-			return False
-		else:
-			 return Poids.λ * self.temps + (1-Poids.λ) * self.distance == Poids.λ * w.temps + (1-Poids.λ) * w.distance
-		
+		return self.valeur == w.valeur
+
 	def __le__(self,w):
 		return self < w or self == w
-		
+
 	def __gt__(self,w):
 		return not (self <= w)
-	
+
 	def __ge__(self,w):
 		return not (self < w)
-	
-	def __str__(self):
-		return "(t{0},d{1})".format(self.temps, self.distance)
 
-ZERO = Poids(0,0)
-INF = Poids(inf,inf)
+	def __str__(self):
+		return "{}".format(self.collection)
+
+ZERO = Poids([0,0,0])
+INF = Poids([inf,inf,inf])
 
 Alphabet = [ chr(65+i) for i in range(50) ]
 
@@ -54,70 +108,21 @@ def Annuaire():
     for i in range(50):
         A[chr(65 + i)] = i
     return A
-    
-P = lambda : Poids(randint(1,6),randint(1,6))
 
-Pays = [
-('A',[('B',P()),('E',P())]),
-('B',[('D',P())]),
-('C',[('B',P()),('D',P()),('E',P())]),
-('F',[('D',P()),('E',P())])
-]
-
+P = lambda : Poids([ randint(1,6) for _ in range(3) ])
 
 Ville = [
 ('A', [('B', P()),('C', P()), ('D', P())]),
 ('D', [('B', P()),('C', P())])
 ]
 
-def Normaliser(s):
-	g = ""
-	build = ""
-	for k in range(len(s)):
-		
-		if s[k].isnumeric():
-			build += s[k]
-		elif build != "":
-			if int(build) > 15:
-				g += str(randint(0,15)) + s[k]
-			else:
-				g+= build + s[k]
-			build = ""
-		else:
-			g += s[k]
-	return g
+Pays = [
+('A',[('B',P()),('E',P())]),
+('B',[('D',P())]),
+('C',[('E',P()),('B',P()),('D',P()),('G',P())]),
+('F',[('D',P()),('G',P())]),
+('G',[('E', P())])
+]
 
-def Lettrifier(s):
-	g = ""
-	build = ""
-	for k in range(len(s)):
-		if s[k].isnumeric():
-			build += s[k]
-		elif build != "":
-			g += Alphabet[int(build)] + s[k]
-			build = ""
-		else:
-			g += s[k]
-	return g
-
-def DotVersGraphe(content):
-	G = []
-	L = content.split('\n\t')
-	L = L[1:-1]
-	for l in Alphabet:
-		Points = []
-		for t in L:
-			if t[0] == l:
-				Points.append((t[-1],P()))
-		if Points != []:
-			G.append( (l,Points) )
-	return G
-
-path = 'Graphe/abstract.gv'
-dot = Source.from_file(path)	
-dot.source = Lettrifier(Normaliser(dot.source))
-Abstrait = DotVersGraphe(dot.source)
-
-
-
+## TEST
 

31f9fc0ff8d40ba765e7789f299f5140b097fbfb
Matman06 <63415111+Matman06@users.noreply.github.com>
2020-10-27 15:29:40 +0100

    Add files via upload

---
+++ interface graphique TIPE.py
@@ -0,0 +1,32 @@
+from tkinter import *
+
+fenetre = Tk()
+
+
+#Choix du type de graphe
+var_choix = StringVar()
+
+choix_DS = Radiobutton(fenetre, text="Déterministe Statique", variable=var_choix, value="DS")
+choix_SS = Radiobutton(fenetre, text="Stochastique Statique", variable=var_choix, value="SS")
+choix_DD = Radiobutton(fenetre, text="Déterministe Dynamique", variable=var_choix, value="DD")
+choix_SD = Radiobutton(fenetre, text="Stochastique Dynamique", variable=var_choix, value="SD")
+
+choix_DS.pack()
+choix_SS.pack()
+choix_DD.pack()
+choix_SD.pack()
+
+#Nombre de noeuds dans le graphe
+nb_noeuds = Label(fenetre, text="Combien de noeuds souhaitez-vous dans le graphe ?")
+nb_noeuds.pack()
+
+var_noeuds = StringVar()
+ligne_noeuds = Entry(fenetre, textvariable = var_noeuds, width = 30)
+ligne_noeuds.pack()
+
+#Valider
+bouton_valider = Button(fenetre, text="Valider")
+bouton_valider.pack()
+
+fenetre.mainloop()
+

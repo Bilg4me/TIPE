9daff1e8f9b7a2f31bd736ad88f221d83a195fdd
Matman06 <63415111+Matman06@users.noreply.github.com>
2020-11-10 23:08:16 +0100

    Update Lecture et création Matrice.py

--- Lecture et création Matrice.py
+++ Lecture et création Matrice.py
@@ -1,13 +1,14 @@
 import xlrd
 import matplotlib.pyplot as plt
 
-Adresse = 'M:\Feuille.xlsx'
+Adresse = '/Users/mathieutanre/Documents/TIPE/Feuille.xlsx'
 document = xlrd.open_workbook(Adresse)
 AdresseDD = 'M:\TIPE-master\Tableur SS.xlsx'
- 
- 
+
+
 Feuille_VillesDS = document.sheet_by_index(0) #Villes et DS
 Feuille_SS = document.sheet_by_index(1) # (Poids,Proba) SS
+Feuille_DD = document.sheet_by_index(2) # Dynamique DD
 
 
 cols1 = Feuille_VillesDS.ncols
@@ -18,36 +19,56 @@ def RécupVilles():
     for r in range(1, rows1):
         ListeVilles += [Feuille_VillesDS.cell_value(rowx=r, colx=0)]
     return ListeVilles
-    
+
 def CréerMatriceDéterministeStatique(): #AKA DS (poids)
     Matrice = [[ None for i in range(rows1 -1) ] for i in range (cols1 - 1)]
     for i in range(1,rows1):
         Matrice[i-1][i-1] = 0
-        j = i + 1 
+        j = i + 1
         while j < cols1:
             Matrice[i-1][j-1] = Feuille_VillesDS.cell_value(rowx=i,colx=j)
             Matrice[j-1][i-1] = Matrice[i-1][j-1] #Matrice symétrique DONC ne fonctionne pas en orienté
             j += 1
     return Matrice
-    
-    
+
+
 cols2 = Feuille_SS.ncols
 rows2 = Feuille_SS.nrows
 
-def CréerMatriceStochastiqueStatique(): #AKA SS (poids + proba)
+n= cols1 - 1
+
+def CréerMatriceStochastiqueStatique1(): #AKA SS (poids + proba)
+    Matrice = [[ None for i in range(n)] for i in range(n)] ; a = 0 ; b = 1 # a numéro de ligne ; b numéro de colonne
+    for k in range(n):
+        Matrice[k][k] = [(0,1)]
+    for j in range (0, n * (n-1) , 2): #Pas de 2 car poids + proba codé sur 2 colonnes
+        i = 0 ; Poids = []
+        while Feuille_SS.cell_value(rowx=i,colx=j) != 0:  #On s'arrête avant out of range
+            Poids.append((Feuille_SS.cell_value(rowx=i,colx=j) , Feuille_SS.cell_value(rowx=i,colx=j+1)))
+            i += 1
+        Matrice[a][b] = Poids
+        Matrice[b][a] = Matrice[a][b]
+        if b < n-1:
+            b += 1
+        else:
+            a += 1
+            b = a + 1
+    return Matrice
+
+def CréerMatriceStochastiqueStatique2(): #AKA SS (poids + proba)
 
     Classeur_DD = xlrd.open_workbook(AdresseDD)
     n = len(Classeur_DD.sheet_names) - 1
     Feuille_Base = Classeur_DD.sheet_by_index(0)
 
     Matrice = [[ None for i in range(cols)] for i in range(ligne)] ; a = 0 ; b = 1 # a numéro de ligne ; b numéro de colonne
-    
+
     for M in range(1,n+1):
         Feuille = Classeur_DD.sheet_by_index(M)
         col = Feuille.ncols - 1 ; ligne = Feuille.nrows - 1
-        
-        
-        for j in range (0,(col) * ( col - 1) // 2, 2): #Pas de 2 car poids + proba codé sur 2 colonnes 
+
+
+        for j in range (0,(col) * ( col - 1) // 2, 2): #Pas de 2 car poids + proba codé sur 2 colonnes
             i = 0 ; Poids = []
             while Feuille.cell_value(rowx=i,colx=j) != 0:  #On s'arrête avant out of range
                 Poids.append((Feuille.cell_value(rowx=i+1,colx=j+1) , Feuille.cell_value(rowx=i+1,colx=j+2)))
@@ -57,10 +78,27 @@ def CréerMatriceStochastiqueStatique(): #AKA SS (poids + proba)
             b += 1
         a += 1
     return Matrice
-        
-        
+
+
+
+
+cols3 = Feuille_DD.ncols
+rows3 = Feuille_DD.nrows
 
 def CréerMatriceDéterministeDynamique(): #AKA DD (poids + heures)
-    ListeListePoids = []
-    
-    
+    Matrice = [[ None for i in range(n)] for i in range(n)] ; a=0 ; b=1
+    for k in range(n) :
+        Matrice[k][k] = [0 for i in range(rows3)]
+    for j in range (cols3): #Pas de 2 car poids + proba codé sur 2 colonnes
+        Poids = []
+        for i in range (rows3):  #On s'arrête avant out of range
+            Poids.append(Feuille_DD.cell_value(rowx=i,colx=j))
+        Matrice[a][b] = Poids
+        Matrice[b][a] = Matrice[a][b]
+        if b < n-1:
+            b += 1
+        else:
+            a += 1
+            b = a + 1
+    return Matrice
+

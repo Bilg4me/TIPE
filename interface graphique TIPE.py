996405a4c624ce483f31809d840c0ac0c1eadc64
brome <bilgame91330@gmail.com>
2020-12-22 23:42:38 +0100

    page d'edition ameliorations

--- interface graphique TIPE.py
+++ interface graphique TIPE.py
@@ -1,21 +1,16 @@
 from tkinter import *
-
-def ModeRoutier():
-	FenetreTypeGraphe("Routier")
-
-def ModeFerroviaire():
-	FenetreTypeGraphe("Ferro")
+from PIL import ImageTk
 
 def FenetreMode():
 	global fenetre
 	fenetre = Tk()
 	fenetre.geometry('800x600')
-	Button(fenetre, text="Routier", bg = "green" , fg = "black", command = ModeRoutier).pack(side=TOP, padx=5, pady=5)
-	Button(fenetre, text="Ferro", bg = "green" , fg = "black", command = ModeFerroviaire).pack(side=BOTTOM, padx=5, pady=5)
+	Button(fenetre, text="Routier", bg = "green" , fg = "black", command = lambda : FenetreTypeGraphe("Routier")).pack(side=TOP, padx=5, pady=5)
+	Button(fenetre, text="Ferro", bg = "green" , fg = "black", command = lambda : FenetreTypeGraphe("Ferro")).pack(side=BOTTOM, padx=5, pady=5)
 
 def FenetreTypeGraphe(mode):
 	
-#Choix du type de graphe
+    #Choix du type de graphe
     var_choix = StringVar()
     
     choix_DS = Radiobutton(fenetre, text="Déterministe Statique", bg = "violet", variable = var_choix, value="DS")
@@ -42,19 +37,113 @@ def FenetreTypeGraphe(mode):
     
 def FenetreModelisation(mode, typeGraphe):
 	fenetre2 = Tk()
-	fenetre2.geometry('350x200')
-	TitreChoix2 = Label(fenetre2,text = "mode : {} avec un type de graphe : {} ".format(mode,typeGraphe) , relief=RAISED , bg = "black" , fg = "white")
-	TitreChoix2.place(x = 0, y = 5)
-	# #Nombre de noeuds dans le graphe
-    # nb_noeuds = Label(fenetre2, text="Combien de noeuds souhaitez-vous dans le graphe ?")
-    # nb_noeuds.place(x = 30 , y = 140)
-    
-    # var_noeuds = StringVar()
-    # ligne_noeuds = Spinbox(fenetre2, from_=0, to=10, variable = var_noeuds)
-    # ligne_noeuds.place(x = 80 , y = 160)
+	fenetre2.geometry('800x600')
+	
+	def alert():
+		return None
+		
+	# Menu
+	menubar = Menu(fenetre2)
 
+	menu1 = Menu(menubar, tearoff=0)
+	menu1.add_command(label="Créer", command=alert)
+	menu1.add_command(label="Importer", command=alert)
+	menu1.add_command(label="Exporter", command=alert)
+	menu1.add_command(label="Sauvegarder", command=alert)
+	menu1.add_separator()
+	menu1.add_command(label="Quitter", command=fenetre.quit)
+	menubar.add_cascade(label="Fichier", menu=menu1)
 
-#Turbo support: https://likegeeks.com/python-gui-examples-tkinter-tutorial/
+	menu2 = Menu(menubar, tearoff=0)
+	menu2.add_command(label="Couper", command=alert)
+	menu2.add_command(label="Copier", command=alert)
+	menu2.add_command(label="Coller", command=alert)
+	menubar.add_cascade(label="Edition", menu=menu2)
+
+	menu3 = Menu(menubar, tearoff=0)
+	menu3.add_command(label="A propos", command=alert)
+	menubar.add_cascade(label="Aide", menu=menu3)
+
+	fenetre2.config(menu=menubar)
+	
+	# previsualisation du graphe
+	
+	def previsualiser():
+		photo = ImageTk.PhotoImage(file="Graphe.jpeg")
+		canvas = Canvas(apercu,width=100, height=500)
+		canvas.create_image(0, 0, anchor=NW, image=photo)
+		canvas.image = photo
+		canvas.pack(fill="both", expand=True)
+		
+	# ajout/suppression de noeuds et arc
+	
+	def ajoutNoeuds():
+		fenetre = Tk()
+		fenetre.geometry('300x300')
+		nb_noeuds = Label(fenetre, text="Combien de noeuds souhaitez-vous ajouter dans le graphe ?")
+		nb_noeuds.place(x = 30 , y = 140)
+		var_noeuds = StringVar()
+		ligne_noeuds = Spinbox(fenetre, from_=0, to=10)
+		ligne_noeuds.place(x = 0 , y = 0)
+		fenetre.mainloop()
+	
+	def supprimeNoeuds(listeDesNoeuds):
+		fenetre = Tk()
+		fenetre.geometry('300x300')
+		listeDesBoutons = [ Checkbutton(fenetre, text= noeuds) for noeuds in listeDesNoeuds ]
+		for checkbox in listeDesBoutons:
+			checkbox.pack()
+		
+		Button(fenetre, text="Supprimer", bg = "red" , fg = "white", command = alert).pack(side = BOTTOM)
+		fenetre.mainloop()
+		
+	def ajoutArcs(listeDesNoeuds):
+		fenetre = Tk()
+		fenetre.geometry('300x300')
+		
+		départ = LabelFrame(fenetre, text="Départ", padx=20, pady=20)
+		départ.pack(fill="y", expand="yes", side = LEFT)
+		
+		listeDesDeparts = Listbox(départ)
+		for noeuds in listeDesNoeuds:
+			listeDesDeparts.insert(1,noeuds)
+
+		listeDesDeparts.pack()
+		
+		arrivée = LabelFrame(fenetre, text="Arrivée", padx=20, pady=20)
+		arrivée.pack(fill="y", expand="yes", side = RIGHT)
+		
+		listeDesArrivées = Listbox(arrivée)
+		for noeuds in listeDesNoeuds:
+			listeDesArrivées.insert(1,noeuds)
+
+		listeDesArrivées.pack()
+		
+		fenetre.mainloop()
+		
+	
+	# Cadre d'édition de graphe
+	
+	editeur = LabelFrame(fenetre2, text="Editeur", padx=20, pady=20)
+	editeur.pack(fill="y", expand="no", side = LEFT)
+	
+	Label(editeur, text = "mode : {} avec un type de graphe : {} ".format(mode,typeGraphe) ).pack()
+	
+	Button(editeur, text="Ajouter noeuds", bg = "green" , fg = "black", command = ajoutNoeuds).pack()
+	Button(editeur, text="Supprimer noeuds", bg = "green" , fg = "black", command = lambda : supprimeNoeuds(['a','b','c','d','e']) ).pack()
+	Button(editeur, text="Ajouter arcs", bg = "green" , fg = "black", command = lambda : ajoutArcs(['a','b','c','d','e'])).pack()
+	Button(editeur, text="Supprimer arcs", bg = "green" , fg = "black", command = alert).pack()
+	Button(editeur, text="Modifier arc", bg = "green" , fg = "black", command = alert).pack()
+	Button(editeur, text="Aperçu", bg = "blue" , fg = "white", command = previsualiser).pack(side = BOTTOM)
+	
+	if mode == "Ferro":
+		Button(editeur, text="Créer ligne", bg = "green" , fg = "black", command = alert).pack()
+	
+	# Cadre aperçu du graphe
+	
+	apercu = LabelFrame(fenetre2, text="Aperçu", padx=20, pady=20)
+	apercu.pack(fill="both", expand=True, side = RIGHT)
+
 
 FenetreMode()
 fenetre.mainloop()

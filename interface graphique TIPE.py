d48b436c965f6080c2fceac43deacf6527666537
brome <bilgame91330@gmail.com>
2020-12-25 20:52:03 +0100

    mode apercu disponible, ajout de sommet et arc fonctionnel, suppression non fonctionnelle

--- interface graphique TIPE.py
+++ interface graphique TIPE.py
@@ -1,37 +1,40 @@
 from tkinter import *
+from Graphes_et_Poids import *
 from PIL import ImageTk
 
 # Graphe
 
-G = [[],[]]
+G = Graphe([],[],Poids)
 
-def setPoids(A,B):
-	if not (A in G[0] and B in G[0]):
-		raise Exception("Désolé, cet arc ne peut etre construit")
+def setPoids(window, A,B):
+	if not (A in G and B in G):
+		raise Exception("Désolé, cet arc ne peut être construit")
+	window.destroy()
 	fenetre = Tk()
 	fenetre.geometry("300x300")
 	def definew(w):
-		G[1].append( [A,B,w] )
+		G.ajouter_arc(A,B,Poids(w))
+		fenetre.destroy()
 	s = Spinbox(fenetre, from_=0, to=10)
 	s.pack()
 	Button(fenetre,text="definir", command= lambda : definew(s.get()) ).pack()
-	
 
 def setName(node):
-	G[0][node] = "changé"
+	G.sommets[node] = "changé"
 	return None
 
 def FenetreMode():
-	global fenetre
 	fenetre = Tk()
 	fenetre.geometry('800x600')
-	Button(fenetre, text="Routier", bg = "green" , fg = "black", command = lambda : FenetreTypeGraphe("Routier")).pack(side=TOP, padx=5, pady=5)
-	Button(fenetre, text="Ferro", bg = "green" , fg = "black", command = lambda : FenetreTypeGraphe("Ferro")).pack(side=BOTTOM, padx=5, pady=5)
+	Button(fenetre, text="Routier", bg = "green" , fg = "black", command = lambda : FenetreTypeGraphe("Routier", fenetre)).pack(side=TOP, padx=5, pady=5)
+	Button(fenetre, text="Ferro", bg = "green" , fg = "black", command = lambda : FenetreTypeGraphe("Ferro", fenetre)).pack(side=BOTTOM, padx=5, pady=5)
 	fenetre.mainloop()
 
-def FenetreTypeGraphe(mode):
-
+def FenetreTypeGraphe(mode, window):
     #Choix du type de graphe
+    window.destroy()
+    fenetre = Tk()
+    fenetre.geometry('800x600')
     var_choix = StringVar()
 
     choix_DS = Radiobutton(fenetre, text="Déterministe Statique", bg = "violet", variable = var_choix, value="DS")
@@ -54,7 +57,6 @@ def FenetreTypeGraphe(mode):
 
     bouton_valider = Button(fenetre, text="Valider", bg = "green" , fg = "black", command = Valider)
     bouton_valider.place(x = 150 , y = 180)
-
 
 def FenetreModelisation(mode, typeGraphe):
 	fenetre = Tk()
@@ -89,8 +91,12 @@ def FenetreModelisation(mode, typeGraphe):
 
 	# previsualisation du graphe
 
-	def previsualiser():
-		photo = ImageTk.PhotoImage(file="Graphe.jpeg")
+	def previsualiser(frame):
+		for widget in frame.winfo_children():
+			widget.destroy()
+
+		G.Visualisation()
+		photo = ImageTk.PhotoImage(file="Graphe.gv.png")
 		canvas = Canvas(apercu,width=100, height=500)
 		canvas.create_image(0, 0, anchor=NW, image=photo)
 		canvas.image = photo
@@ -103,7 +109,7 @@ def FenetreModelisation(mode, typeGraphe):
 			window.destroy()
 			def ajouterlesnoeudsaugraphe(window, entrees):
 				for v in entrees:
-					G[0].append(v.get())
+					G.ajouter_sommet(v.get())
 				window.destroy()
 
 			fenetre = Tk()
@@ -135,28 +141,31 @@ def FenetreModelisation(mode, typeGraphe):
 		Button(fenetre, text="Supprimer", bg = "red" , fg = "white", command = fenetre.destroy).pack(side = BOTTOM)
 		fenetre.mainloop()
 
+	# Ajouts, suppression arcs
+
 	def ajoutArcs(listeDesNoeuds):
 		fenetre = Tk()
 		fenetre.geometry('300x300')
-		
+
 		Label(fenetre, text="depart").pack()
-		
+
 		pointdepart = Entry(fenetre)
 		pointdepart.pack()
-		
+
 		Label(fenetre, text="arrive").pack()
-		
+
 		pointarrivee = Entry(fenetre)
 		pointarrivee.pack()
-			
-		Button(fenetre, text="Set poids", command= lambda : setPoids(pointdepart.get(), pointarrivee.get())).pack()
+
+		Button(fenetre, text="Set poids", command= lambda : setPoids(fenetre, pointdepart.get(), pointarrivee.get())).pack()
 
 		fenetre.mainloop()
 
 	def supprimeArcs(listeDesArcs):
 		fenetre = Tk()
 		fenetre.geometry('300x300')
-		listeDesBoutons = [ Checkbutton(fenetre, text=arcs) for arcs in listeDesArcs ]
+
+		listeDesBoutons = [ Checkbutton(fenetre, text=str(arcs)) for arcs in listeDesArcs ]
 		for checkbox in listeDesBoutons:
 			checkbox.pack()
 
@@ -172,7 +181,6 @@ def FenetreModelisation(mode, typeGraphe):
 				liste.insert(1,noeuds)
 			liste.pack()
 			Button(fenetre2, text="Modifier le nom du sommet", command= lambda : setName(liste.curselection()[0])).pack()
-
 
 		def changePoidsArc(listeDesArcs):
 			fenetre2 = Tk()
@@ -198,14 +206,32 @@ def FenetreModelisation(mode, typeGraphe):
 	editeur = LabelFrame(fenetre, text="Editeur", padx=20, pady=20)
 	editeur.pack(fill="y", expand="no", side = LEFT)
 
-	Label(editeur, text = "mode : {} avec un type de graphe : {} ".format(mode,typeGraphe) ).pack()
+	fenetre.title("mode : {} avec un type de graphe : {} ".format(mode,typeGraphe))
+	
+	#cas des graphes Dynamiques
+	periode = None
+	cycle = None
+	if typeGraphe[-1] == 'D':
+		parametresDynamique = Tk()
+		parametresDynamique.geometry('400x400')
+		Label(parametresDynamique,text = " Définir la durée d'une période ", bg = "black" , fg = "white").pack()
+		periodeEntree = Entry(parametresDynamique)
+		periodeEntree.pack()
+		Label(parametresDynamique,text = " Définir le nombre de période ", bg = "black" , fg = "white").pack()
+		nbdeperiodeEntree = Entry(parametresDynamique)
+		nbdeperiodeEntree.pack()
+		def recupererParametreDynamique():
+			periode = int(periodeEntree.get())
+			cycle = periode * int(nbdeperiodeEntree.get())
+			parametresDynamique.destroy()
+			Label(editeur, text = "Dynamique settings : Periode {} u.t / Cycle {} u.t".format(periode,cycle)).pack()
+		Button(parametresDynamique, text = "valider", command=recupererParametreDynamique).pack()
 
 	Button(editeur, text="Ajouter noeuds", bg = "green" , fg = "black", command = ajoutNoeuds).pack()
-	Button(editeur, text="Supprimer noeuds", bg = "green" , fg = "black", command = lambda : supprimeNoeuds(G[0]) ).pack()
-	Button(editeur, text="Ajouter arcs", bg = "green" , fg = "black", command = lambda : ajoutArcs(G[0])).pack()
-	Button(editeur, text="Supprimer arcs", bg = "green" , fg = "black", command = lambda : supprimeArcs(G[1])).pack()
-	Button(editeur, text="Modifier arc", bg = "green" , fg = "black", command = lambda : modifierArc(G[0],G[1]) ).pack()
-	Button(editeur, text="Aperçu", bg = "blue" , fg = "white", command = previsualiser).pack(side = BOTTOM)
+	Button(editeur, text="Supprimer noeuds", bg = "green" , fg = "black", command = lambda : supprimeNoeuds(G.sommets) ).pack()
+	Button(editeur, text="Ajouter arcs", bg = "green" , fg = "black", command = lambda : ajoutArcs(G.sommets)).pack()
+	Button(editeur, text="Supprimer arcs", bg = "green" , fg = "black", command = lambda : supprimeArcs(G.arcs)).pack()
+	Button(editeur, text="Modifier arc", bg = "green" , fg = "black", command = lambda : modifierArc(G.sommets,G.arcs) ).pack()
 
 	if mode == "Ferro":
 		Button(editeur, text="Créer ligne", bg = "green" , fg = "black", command = alert).pack()
@@ -215,9 +241,9 @@ def FenetreModelisation(mode, typeGraphe):
 	apercu = LabelFrame(fenetre, text="Aperçu", padx=20, pady=20)
 	apercu.pack(fill="both", expand=True, side = RIGHT)
 
+	Button(editeur, text="Aperçu", bg = "blue" , fg = "white", command = lambda : previsualiser(apercu) ).pack(side = BOTTOM)
+
 	fenetre.mainloop()
 
-
 # Phase de tests
-
 FenetreMode()
